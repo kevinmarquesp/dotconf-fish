2e1e0e82b792a26eb5ac8aa24168a35fa19d58d6
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-29 20:26:28 -0300

    :sparkles: Now the main config file can import all of that!

---
+++ config.fish
@@ -0,0 +1,24 @@
+set fish_greeting
+
+set -g DESKTOP "$HOME/Desktop"
+
+source "$HOME/.config/fish/base/varsrc.fish"
+source "$HOME/.config/fish/base/aliasrc.fish"
+source "$HOME/.config/fish/base/keybinds.fish"
+source "$HOME/.config/fish/base/functions.fish"
+
+fish_vi_key_bindings  #enambe vi mode
+
+## Daqui pra baixo, só vai ter linhas que outros programas inserem aqui
+## --------------------------------------------------------------------
+
+test -f "$HOME/.asdf/asdf.fish" &&
+    source "$HOME/.asdf/asdf.fish"
+
+set -gx PNPM_HOME "/home/kevin/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+
+test -f "/home/kevin/.xmake/profile" &&
+    source "/home/kevin/.xmake/profile"
