06eed863c08817e073e92e753433de74d519da29
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-12-31 10:11:59 -0300

    :sparkles: Added the fisher plugin manager

--- config.fish
+++ config.fish
@@ -8,7 +8,16 @@ source "$G_FISH_CONFIG/user/aliasrc.fish"
 source "$G_FISH_CONFIG/user/keybinds.fish"
 source "$G_FISH_CONFIG/user/functions.fish"
 
+function fish_mode_prompt; end  #remove the mode indicator
 fish_vi_key_bindings  #enambe vi mode
+
+if [ ! -f "$G_FISH_CONFIG/functions/fisher.fish" ]  #install fisher for me
+    mkdir -p "$G_FISH_CONFIG/functions"
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish > "$G_FISH_CONFIG/functions/fisher.fish"
+
+    source "$G_FISH_CONFIG/config.fish"
+    fisher install jorgebucaran/fisher
+end
 
 ## Daqui pra baixo, só vai ter linhas que outros programas inserem aqui
 ## --------------------------------------------------------------------
